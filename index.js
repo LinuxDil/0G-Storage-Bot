2b82e4f67f485124d45c604918c6fae90cffdb35
Jack Sparrow <133944655+LinuxDil@users.noreply.github.com>
2025-05-15 14:42:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const logger = {
 
 const CHAIN_ID = 16601;
 const RPC_URL = 'https://evmrpc-testnet.0g.ai';
-const CONTRACT_ADDRESS = '0x56A565685C9992BF5ACafb940ff68922980DBBC5';
+const CONTRACT_ADDRESS = '0x5f1d96895e442fc0168fa2f9fb1ebef93cb5035e';
 const METHOD_ID = '0xef3e12dc';
 const PROXY_FILE = 'proxies.txt';
 const INDEXER_URL = 'https://indexer-storage-testnet-turbo.0g.ai';
